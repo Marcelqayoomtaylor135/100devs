4d7987152676da7b47ba35acfca5a115594f10f0
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-06-16 17:48:51 +1000

    Add objects-shots

--- JS/class26-materials/objects-shots/js/main.js
+++ JS/class26-materials/objects-shots/js/main.js
@@ -1,1 +1,25 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+// Add event listener to get cocktail button
+// Take input and fetch that cocktail
+// set name photo and instructions
+
+
+document.querySelector("button").addEventListener('click', fetchCocktail)
+
+function fetchCocktail(){
+    let cocktail = document.querySelector("input").value
+    console.log(`cocktail was ${cocktail}`)
+    fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector("h2").innerHTML =  data.drinks[0].strDrink 
+            document.querySelector("img").src = data.drinks[0].strDrinkThumb
+            document.querySelector("h3").innerHTML = data.drinks[0].strInstructions
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+    });
+}
+
